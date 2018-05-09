a12049b84fde00ef3c0b55d92453e30a17dac2da
giiita <takagi_ryota@abema.tv>
2018-05-09 20:09:59 +0900

    Initialize all disabled.

--- src/main/scala/com/giitan/container/AutomaticContainerInitializer.scala
+++ src/main/scala/com/giitan/container/AutomaticContainerInitializer.scala
@@ -6,9 +6,6 @@ import scala.reflect.ClassTag
 import scala.reflect.runtime.universe._
 
 private[giitan] object AutomaticContainerInitializer {
-
-  implicitly[Container].automaticDependencies.initialize()
-
   def initialize[T: ClassTag](tag: TypeTag[T]): Unit = {
     implicitly[Container].automaticDependencies.initialize(tag)
   }
