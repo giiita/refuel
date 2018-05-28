c10208547f36e9d022baafc00cf81d32876bae68
giiita <takagi_ryota@abema.tv>
2018-05-28 16:39:02 +0900

    AutoInject be class.

--- src/main/scala/com/giitan/injector/AutoInject.scala
+++ src/main/scala/com/giitan/injector/AutoInject.scala
@@ -4,11 +4,7 @@ import com.giitan.container.FunctIndexer
 import com.giitan.scope.Scope
 import scala.reflect.runtime.universe._
 
-class AutoInjectThroughTag[X: TypeTag] extends AutoInjector { me: X =>
+class AutoInject[X: TypeTag] extends AutoInjector { me: X =>
   FunctIndexer.indexing(typeTag[X], me, me)
   Scope[X].acceptedGlobal()
-}
-
-trait AutoInject[X] extends AutoInjectThroughTag[X] {
-  me: X =>
 }
