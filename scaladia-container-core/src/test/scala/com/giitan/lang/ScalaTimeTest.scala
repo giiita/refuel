1ebe16d1e4af1a9689f065b86a7409892c75f733
giiita <takagi_ryota@abema.tv>
2019-02-14 11:35:42 +0900

    Add settings.

--- scaladia-container-core/src/test/scala/com/giitan/lang/ScalaTimeTest.scala
+++ scaladia-container-core/src/test/scala/com/giitan/lang/ScalaTimeTest.scala
@@ -9,50 +9,52 @@ import org.scalatest.{Matchers, WordSpec}
 
 class ScalaTimeTest extends WordSpec with Matchers with Injector {
 
-  depends[RuntimeTZ](
-    new RuntimeTZ {
-      override val TIME_ZONE: TimeZone = TimeZone.getTimeZone("Asia/Tokyo")
-      override val ZONE_ID: ZoneId = ZoneId.of("Asia/Tokyo")
-      override val ZONE_OFFSET: ZoneOffset = ZoneOffset.ofHours(9)
-      override val DEFAULT_FORMAT: String = RuntimeTZ.DEFAULT_FORMAT
-    }
-  )
+  trait Context {
+    depends[RuntimeTZ](
+      new RuntimeTZ {
+        override val TIME_ZONE: TimeZone = TimeZone.getTimeZone("Asia/Tokyo")
+        override val ZONE_ID: ZoneId = ZoneId.of("Asia/Tokyo")
+        override val ZONE_OFFSET: ZoneOffset = ZoneOffset.ofHours(9)
+        override val DEFAULT_FORMAT: String = RuntimeTZ.DEFAULT_FORMAT
+      }
+    )
+  }
 
   "ZonedDateTimeBs" should {
-    "parse yyyy/MM/dd HH:mm:ss" in {
+    "parse yyyy/MM/dd HH:mm:ss" in new Context {
       "2017/08/26 11:33:54".datetime shouldBe ZonedDateTime.of(2017, 8, 26, 11, 33, 54, 0, RuntimeTZ.ZONE_ID)
     }
-    "parse yyyyMMddHHmmss" in {
+    "parse yyyyMMddHHmmss" in new Context {
       "20170826113354".datetime shouldBe ZonedDateTime.of(2017, 8, 26, 11, 33, 54, 0, RuntimeTZ.ZONE_ID)
     }
-    "parse yyyy/MM/dd" in {
+    "parse yyyy/MM/dd" in new Context {
       "2017/08/26".datetime shouldBe ZonedDateTime.of(2017, 8, 26, 0, 0, 0, 0, RuntimeTZ.ZONE_ID)
     }
-    "parse yyyyMMdd" in {
+    "parse yyyyMMdd" in new Context {
       "20170826".datetime shouldBe ZonedDateTime.of(2017, 8, 26, 0, 0, 0, 0, RuntimeTZ.ZONE_ID)
     }
-    "parse yyyy/M/d" in {
+    "parse yyyy/M/d" in new Context {
       "2017/8/26".datetime shouldBe ZonedDateTime.of(2017, 8, 26, 0, 0, 0, 0, RuntimeTZ.ZONE_ID)
     }
-    "parse yyyy/M/d H" in {
+    "parse yyyy/M/d H" in new Context {
       "2017/8/26 9".datetime shouldBe ZonedDateTime.of(2017, 8, 26, 9, 0, 0, 0, RuntimeTZ.ZONE_ID)
     }
-    "parse yyyy-MM-dd HH:mm:ss" in {
+    "parse yyyy-MM-dd HH:mm:ss" in new Context {
       "2017-08-26 11:33:54".datetime shouldBe ZonedDateTime.of(2017, 8, 26, 11, 33, 54, 0, RuntimeTZ.ZONE_ID)
     }
-    "parse yyyy-MM-dd" in {
+    "parse yyyy-MM-dd" in new Context {
       "2017-08-26".datetime shouldBe ZonedDateTime.of(2017, 8, 26, 0, 0, 0, 0, RuntimeTZ.ZONE_ID)
     }
-    "parse yyyy-M-d" in {
+    "parse yyyy-M-d" in new Context {
       "2017-8-26".datetime shouldBe ZonedDateTime.of(2017, 8, 26, 0, 0, 0, 0, RuntimeTZ.ZONE_ID)
     }
-    "parse yyyy-M-d H" in {
+    "parse yyyy-M-d H" in new Context {
       "2017-8-26 9".datetime shouldBe ZonedDateTime.of(2017, 8, 26, 9, 0, 0, 0, RuntimeTZ.ZONE_ID)
     }
-    "parse yyyy-M-d HH" in {
+    "parse yyyy-M-d HH" in new Context {
       "2017-8-26 09".datetime shouldBe ZonedDateTime.of(2017, 8, 26, 9, 0, 0, 0, RuntimeTZ.ZONE_ID)
     }
-    "parse unexpected" in {
+    "parse unexpected" in new Context {
       try {
         "2017826".datetime
       } catch {
@@ -63,51 +65,51 @@ class ScalaTimeTest extends WordSpec with Matchers with Injector {
   }
 
   "StringBs" should {
-    "as default" in {
+    "as default" in new Context {
       "2017/08/26 11:33:54".datetime.format() shouldBe "2017/8/26 11:33:54 +0900"
     }
-    "as custom" in {
+    "as custom" in new Context {
       "2017/08/26 11:33:54".datetime.format("yyMd") shouldBe "17826"
     }
 
-    "minToday" in {
+    "minToday" in new Context {
       "2017/08/26 11:33:54".datetime.minToday shouldBe ZonedDateTime.of(2017, 8, 26, 0, 0, 0, 0, RuntimeTZ.ZONE_ID)
     }
 
-    "minTohour" in {
+    "minTohour" in new Context {
       "2017/08/26 11:33:54".datetime.minTohour shouldBe ZonedDateTime.of(2017, 8, 26, 11, 0, 0, 0, RuntimeTZ.ZONE_ID)
     }
 
-    "maxToday" in {
+    "maxToday" in new Context {
       "2017/08/26 11:33:54".datetime.maxToday shouldBe ZonedDateTime.of(2017, 8, 26, 23, 59, 59, 0, RuntimeTZ.ZONE_ID)
     }
 
-    "maxTohour" in {
+    "maxTohour" in new Context {
       "2017/08/26 11:33:54".datetime.maxTohour shouldBe ZonedDateTime.of(2017, 8, 26, 11, 59, 59, 0, RuntimeTZ.ZONE_ID)
     }
 
-    "unixtime" in {
+    "unixtime" in new Context {
       "2017/08/26 11:33:54".datetime.unixtime shouldBe ZonedDateTime.of(2017, 8, 26, 11, 33, 54, 0, RuntimeTZ.ZONE_ID).toEpochSecond
     }
   }
 
   "UnixTimeBs" should {
-    "as" in {
+    "as" in new Context {
       ZonedDateTime.of(2017, 8, 26, 11, 33, 54, 0, RuntimeTZ.ZONE_ID).toEpochSecond.datetime shouldBe ZonedDateTime.of(2017, 8, 26, 11, 33, 54, 0, RuntimeTZ.ZONE_ID)
     }
   }
 
   "LocalDateTimeBs" should {
-    "toEpochSec" in {
+    "toEpochSec" in new Context {
       LocalDateTime.of(2017, 8, 26, 11, 33, 54)
         .toEpochSec shouldBe ZonedDateTime
         .of(2017, 8, 26, 11, 33, 54, 0, ZoneId.of("Asia/Tokyo")).toEpochSecond
     }
-    "format" in {
+    "format" in new Context {
       LocalDateTime.of(2017, 8, 26, 11, 33, 54)
         .format() shouldBe "2017/8/26 11:33:54 +0900"
     }
-    "toZonedDateTime" in {
+    "toZonedDateTime" in new Context {
       LocalDateTime.of(2017, 8, 26, 11, 33, 54)
         .toZonedDateTime shouldBe ZonedDateTime
         .of(2017, 8, 26, 11, 33, 54, 0, ZoneId.of("Asia/Tokyo"))
