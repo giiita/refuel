45db66f6a356bfeccfe7bcc1de3d4e7f231c3365
giita <r.takagi4263@gmail.com>
2019-07-11 02:56:26 +0900

    Fix v2.2.0

--- scaladia-lang/version.sbt
+++ scaladia-lang/version.sbt
@@ -1,1 +1,1 @@
-version in ThisProject := "1.1.2"
+version in ThisProject := "1.2.0"
--- scaladia-macro/src/main/scala/com/phylage/scaladia/internal/LazyInitializer.scala
+++ scaladia-macro/src/main/scala/com/phylage/scaladia/internal/LazyInitializer.scala
@@ -1,7 +1,7 @@
 package com.phylage.scaladia.internal
 
 import com.phylage.scaladia.container.Container
-import com.phylage.scaladia.injector.InjectionPool
+import com.phylage.scaladia.injector.{InjectionPool, InjectionType}
 import com.phylage.scaladia.provider.Lazy
 
 import scala.reflect.macros.blackbox
@@ -10,26 +10,42 @@ class LazyInitializer[C <: blackbox.Context](val c: C) {
 
   import c.universe._
 
-  def lazyInit[T: C#WeakTypeTag](fire: c.Expr[Unit], ctn: Tree, ip: Tree, access: c.Tree): Expr[Lazy[T]] = {
+  def lazyInit[T: C#WeakTypeTag](ctn: Tree, ip: Tree, access: c.Tree): Expr[Lazy[T]] = {
+
     c.Expr[Lazy[T]](
       q"""
-         new com.phylage.scaladia.provider.Lazy[${weakTypeOf[T]}] {
-           lazy val provide: ${weakTypeOf[T]} = try {
-             ${injection[T](ctn, ip, fire, access)}
-           } catch {
-             case e: java.lang.Throwable =>
-               throw new com.phylage.scaladia.exception.DIAutoInitializationException(${weakTypeOf[T].typeSymbol.fullName} + " or its internal initialize failed.", e)
-           }
-         }
+        {
+         ${publish(ip)}
+          new com.phylage.scaladia.provider.Lazy[${weakTypeOf[T]}] {
+            lazy val provide: ${weakTypeOf[T]} = try {
+              ${injection[T](ctn, ip, access)}
+            } catch {
+              case e: java.lang.Throwable =>
+                throw new com.phylage.scaladia.exception.DIAutoInitializationException(${weakTypeOf[T].typeSymbol.fullName} + " or its internal initialize failed.", e)
+            }
+          }
+        }
        """
     )
   }
 
-  def diligentInit[T: C#WeakTypeTag](fire: c.Expr[Unit], ctn: Tree, ip: Tree, access: c.Tree): Expr[T] = {
-    injection[T](ctn, ip, fire, access)
+  def diligentInit[T: C#WeakTypeTag](ctn: Tree, ip: Tree, access: c.Tree): Expr[T] = {
+    injection[T](ctn, ip, access)
   }
 
-  def injection[T: C#WeakTypeTag](ctn: Tree, ip: Tree, fire: c.Expr[Unit], access: c.Tree): Expr[T] = {
+  def publish(ip: Tree): Expr[() => Unit] = {
+    c.Expr(
+      q"""
+        import scala.language.experimental.macros
+        def _scrp(): Iterable[com.phylage.scaladia.injector.InjectionType[_]] = macro com.phylage.scaladia.internal.Macro.scrapeInjectionTypes
+        ${c.Expr[InjectionPool](ip)}.pool(
+          () => _scrp()
+        )
+      """
+    )
+  }
+
+  def injection[T: C#WeakTypeTag](ctn: Tree, ip: Tree, access: c.Tree): Expr[T] = {
     val tag = weakTypeOf[T]
 
     val mayBeInjection = c.Expr[Option[T]](
@@ -42,7 +58,6 @@ class LazyInitializer[C <: blackbox.Context](val c: C) {
       q"""
        $mayBeInjection orElse {
          val _pool = ${c.Expr[InjectionPool](ip)}
-         $fire
          _pool.collect[$tag].map(_.apply())
          $mayBeInjection
        } getOrElse {
