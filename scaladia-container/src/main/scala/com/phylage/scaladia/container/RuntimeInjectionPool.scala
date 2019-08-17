4289a763e766e2a0199d21ed21f3a52fb44214ff
giita <r.takagi4263@gmail.com>
2019-08-17 18:15:46 +0900

    Debuging Effective injection.

--- scaladia-container/src/main/scala/com/phylage/scaladia/container/RuntimeInjectionPool.scala
+++ scaladia-container/src/main/scala/com/phylage/scaladia/container/RuntimeInjectionPool.scala
@@ -1,24 +1,15 @@
 package com.phylage.scaladia.container
 
+import com.phylage.scaladia.injector.AutoInject
 import com.phylage.scaladia.injector.InjectionPool.InjectionApplyment
-import com.phylage.scaladia.injector.{AutoInject, InjectionType}
 import com.phylage.scaladia.runtime.{InjectionReflector, RuntimeAutoDIExtractor}
 
 import scala.reflect.runtime.universe
 
-trait RuntimeInjectionPool extends com.phylage.scaladia.injector.InjectionPool {
+object RuntimeInjectionPool extends com.phylage.scaladia.injector.InjectionPool {
 
   private[this] val buffer: Vector[universe.Symbol] = RuntimeAutoDIExtractor.run()
 
-  /**
-    * Pool Injectable subtypes for automatic loading.
-    * The timing to be initialized is when the related
-    * component is initialized or when it is called by inject.
-    *
-    * @param applyer injection object
-    * @return
-    */
-  def pool(applyer: () => Iterable[InjectionType[_]]): Unit
 
   /**
     * Get a list of injection-enabled declarations of any type
