56348a5f01884cb143e41a2d41f28cd9b02dcd3a
giiita <takagi_ryota@abema.tv>
2019-09-20 17:18:13 +0900

    AutoInjectCustomPriority be deprecated.

--- scaladia-container/src/main/scala/com/phylage/scaladia/injector/AutoInjectCustomPriority.scala
+++ scaladia-container/src/main/scala/com/phylage/scaladia/injector/AutoInjectCustomPriority.scala
@@ -4,6 +4,7 @@ package com.phylage.scaladia.injector
   * Inject automatically by specifying priority
   * @tparam T Type to register
   */
+@deprecated("Define and use traits with custom priorities as needed")
 class AutoInjectCustomPriority[T](override val injectionPriority: Int) extends AutoInject[T] { me: T =>
 
 }
