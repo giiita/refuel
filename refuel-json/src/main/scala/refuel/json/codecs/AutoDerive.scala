11748ae90ebacf90ee713bdc980cb7d2cd570f68
giita <r.takagi4263@gmail.com>
2020-07-14 21:30:25 +0900

    Support AutoDerive inheritation.

--- refuel-json/src/main/scala/refuel/json/codecs/AutoDerive.scala
+++ refuel-json/src/main/scala/refuel/json/codecs/AutoDerive.scala
@@ -3,7 +3,9 @@ package refuel.json.codecs
 import refuel.internal.json.CaseCodecFactory
 import refuel.json.{Codec, JsonVal}
 
-object AutoDerive {
+object AutoDerive extends AutoDerive
+
+trait AutoDerive {
   implicit def autoDerivation[T]: Codec[T] = macro CaseCodecFactory.fromInferOrCase[T]
 
   implicit def __as[V](v: V)(implicit c: Write[V]): JsonVal = c.serialize(v)
