3c384884659071c2d9eda9bf69db59d0f669396c
giita <r.takagi4263@gmail.com>
2020-07-31 22:50:50 +0900

    Fix test

--- refuel-json/src/test/scala/refuel/json/JsonTransformTest.scala
+++ refuel-json/src/test/scala/refuel/json/JsonTransformTest.scala
@@ -21,7 +21,7 @@ class JsonTransformTest extends AsyncWordSpec with Matchers with Diagrams with J
     "fail case - Syntax error" in {
       intercept[IllegalJsonFormat] {
         s"""{"value: "aaa"}""".jsonTree
-      }.getMessage shouldBe s"""Unspecified json value of key: #"value: "#"""
+      }.getMessage shouldBe s"""Unspecified json value of key: #"value:"#"""
     }
     "fail case - EOF position 2" in {
       intercept[IllegalJsonFormat] {
