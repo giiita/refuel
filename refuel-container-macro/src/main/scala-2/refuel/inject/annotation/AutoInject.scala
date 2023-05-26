30a43c7774807187cc1bd982035e844bef42649c
giiita <r.takagi4263@gmail.com>
2023-05-27 00:39:49 +0900

    Fix compile

--- refuel-container-macro/src/main/scala-2/refuel/inject/annotation/AutoInject.scala
+++
@@ -1,8 +0,0 @@
-package refuel.inject.annotation
-
-import java.lang.annotation.{Retention, RetentionPolicy, Target}
-import scala.annotation.StaticAnnotation
-
-
-@Retention(RetentionPolicy.SOURCE)
-class AutoInject extends StaticAnnotation
